1d9c776e13266b7ea20360440014d83a6851517c
Sankalp <sankalppatel38@gmail.com>
2022-08-29 16:38:42 +0530

    Update

--- scripts/searchbox.js
+++ scripts/searchbox.js
@@ -1,3 +1,62 @@
+async function Prox()
+{
+    let response = await SearchPhotos();
+    let data = await response;
+    Append(data);
+    window.location.href = "searchbox.html"
+}
+let ID;
+async function SearchPhotos()
+{
+    try
+    {
+      let query = document.getElementById("query").value;
+      let url = `https://api.unsplash.com/search/photos?query=${query}&client_id=nq09tWXzARwU8KjggUE_XnNLnGWL_evJwQuoDawY-Vo`
+      let res = await fetch(url);
+      let data = await res.json();
+      return data.results;
+      
+    }
+    catch(e){
+        console.log(e);
+    }
+};
+
+function Append(data)
+{
+    localStorage.setItem("query",JSON.stringify(data));
+    if(data == undefined)
+    {
+        return false;
+    }
+
+    data.forEach(function(elem)
+    {
+        let box= document.createElement("div")
+          box.className = "collection_tab";
+
+        let p= document.createElement("p")
+        p.innerText = elem.alt_description
+    
+        let poster=document.createElement("img")
+        poster.src=elem.urls.regular;
+
+        box.append(poster,p)
+    })
+}
+
+function debounce(fun,delay)
+{
+    if(ID)
+    {
+        clearTimeout(ID);
+    }
+    ID = setTimeout(function()
+    {
+        fun()
+    },delay)
+}
+
 
 let Attach_Photos=document.getElementById('photos');
 let Collection_Arr = JSON.parse(localStorage.getItem("query"));
